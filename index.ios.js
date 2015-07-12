f0321bbaf1bcccc82651ee52d21d6d11457c3536
Nick Collings <nick@collingo.com>
2015-07-12 14:17:20 +0100

    First experiments

---
+++ index.ios.js
@@ -0,0 +1,10 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var { AppRegistry } = React;
+
+AppRegistry.registerComponent('thinkdo', () => require('./src/node_modules/components/app'));
